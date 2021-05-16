dcdc21fbfb65ae332667cda08910b47e48d935a1
David Vasek <david.vasek51@gmail.com>
2021-05-16 18:54:48 -0500

    week02 update

---
+++ week02.py
@@ -0,0 +1,3 @@
+# Week two at DigitalCrafts
+Time really seems like it is flying already. Week two has already come to an end and we are finishing up our Python projects as I type this. 
+An even larger information load was dumped on us this week and it was definitely a lot to take in. I still am a little confident-lacking in some of the information but overall I thinked i was really able to incorporate what we learned into our projects. 
